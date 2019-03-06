57a8b2ff8f052f221cc47c57d553f1dabd67d0f8
zdevt <zhangtao.hawk@qq.com>
2019-03-06 14:33:53 +0800

    add 1_1 1_2

--- 001.py
+++ 001.py
@@ -1,23 +1,15 @@
-import numpy as np
-import tensorflow as tf
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#       FileName:  001.py
+#
+#    Description:
+#
+#        Version:  1.0
+#        Created:  2019-03-05 15:50:29
+#  Last Modified:  2019-03-05 15:52:01
+#       Revision:  none
+#       Compiler:  gcc
+#
+#         Author:  zt ()
+#   Organization:
 
-x_data = np.random.rand( 100 ).astype( np.float32 )
-y_data = x_data * 0.1 + 0.3
-
-weight = tf.Variable( tf.random_uniform( [1], -1.0, 1.0 ) )
-biases = tf.Variable( tf.zeros( [1] ) )
-y = weight * x_data + biases
-
-loss = tf.reduce_mean( tf.square( y - y_data ) )
-optimizer = tf.train.GradientDescentOptimizer( 0.5 )
-train = optimizer.minimize( loss )
-init = tf.global_variables_initializer( )
-
-sess = tf.Session( )
-
-sess.run( init )
-
-for step in range( 400 ):
-    sess.run( train )
-    if step % 20 == 0:
-        print(step, sess.run( weight ), sess.run( biases ))
